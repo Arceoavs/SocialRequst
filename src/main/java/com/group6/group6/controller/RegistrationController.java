4e6c15bf0cb949a28f05ddf344d0346ff61067f2
ClaasCas <claascassens@t-online.de>
2020-05-13 16:53:18 +0200

    Added Registration Functionality

--- src/main/java/com/group6/group6/controller/RegistrationController.java
+++ src/main/java/com/group6/group6/controller/RegistrationController.java
@@ -1,18 +1,57 @@
 package com.group6.group6.controller;
 
+import com.group6.group6.exception.DuplicateUserException;
 import com.group6.group6.model.User;
+import com.group6.group6.repository.UserRepository;
+import com.group6.group6.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 
+
 public class RegistrationController {
+
+    @Autowired
+    private UserService userService;
+
+    /*
+     * Deliver registration page
+     */
     @GetMapping("/users/register")
     public String showRegistrationForm(WebRequest request, Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "register";
+    }
+
+    /*
+     * Process registration form
+     */
+    @PostMapping("/users/register")
+    public ModelAndView registerUserAccount(@ModelAttribute("user") @Valid User user,
+            HttpServletRequest request) {
+
+        System.out.println("Received registration post for user: " + user.getUsername());
+
+        try {
+            User registered = userService.registerNewUserAccount(user);
+        } catch (DuplicateUserException duEx) {
+            ModelAndView mav = new ModelAndView("register");
+            mav.addObject("message", "An account for that username/email already exists.");
+            return mav;
+        }
+
+        return new ModelAndView("registerSuccess", "user", user);
     }
 }
---
+++ src/main/java/com/group6/group6/service/UserService.java
@@ -0,0 +1,33 @@
+package com.group6.group6.service;
+
+import com.group6.group6.exception.DuplicateUserException;
+import com.group6.group6.model.User;
+import com.group6.group6.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public User registerNewUserAccount(User user) throws DuplicateUserException {
+
+        if (emailExists(user.getEmail()) || usernameExists(user.getUsername())) {
+            throw new DuplicateUserException(
+                    "There is an account with that email or username address: " + user.getEmail() + " / " + user.getUsername());
+        }
+        return userRepository.save(user);
+    }
+
+    private boolean emailExists(String email) {
+        return userRepository.findByEmail(email) != null;
+    }
+
+    private boolean usernameExists(String username) {
+        return userRepository.findByUsername(username) != null;
+    }
+}
