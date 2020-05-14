fec99cc96edcc37719a407cd01f54d4a6652f656
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-05-14 08:57:38 +0200

    distinguish between duplicate email and duplicate username

--- src/main/java/com/group6/group6/service/UserService.java
+++ src/main/java/com/group6/group6/service/UserService.java
@@ -3,41 +3,41 @@ package com.group6.group6.service;
 import com.group6.group6.exception.DuplicateUserException;
 import com.group6.group6.model.User;
 import com.group6.group6.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Transactional
-    public User registerNewUserAccount(User user) throws DuplicateUserException {
-
-        if (emailExists(user.getEmail()) || usernameExists(user.getUsername())) {
-            throw new DuplicateUserException(
-                    "There is an account with that email or username address: " + user.getEmail() + " / " + user.getUsername());
-        }
-
-        // encrypt user password
-        String password = (new BCryptPasswordEncoder()).encode(user.getPassword());
-        user.setPassword(password);
-        user.setPasswordConfirmation(password);
-
-        return userRepository.save(user);
+  @Autowired
+  private UserRepository userRepository;
+ 
+  @Transactional
+  public User registerNewUserAccount(User user) throws DuplicateUserException { 
+    if (emailExists(user.getEmail())) {
+      throw new DuplicateUserException("An account with that email already exists: " + user.getEmail());
     }
 
-    private boolean emailExists(String email) {
-        return userRepository.findByEmail(email) != null;
+    if (usernameExists(user.getUsername())) {
+      throw new DuplicateUserException("An account with that username already exists: " + user.getUsername());
     }
 
-    private boolean usernameExists(String username) {
-        return userRepository.findByUsername(username) != null;
-    }
+    String encryptedPassword = (new BCryptPasswordEncoder()).encode(user.getPassword());
+    user.setPassword(encryptedPassword);
+    user.setPasswordConfirmation(encryptedPassword);
+    
+    return userRepository.save(user);
+  }
+
+  private boolean emailExists(String email) {
+    return userRepository.findByEmail(email) != null;
+  }
+
+  private boolean usernameExists(String username) {
+    return userRepository.findByUsername(username) != null;
+  }
 
 }
