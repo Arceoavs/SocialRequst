dd14a70c7128c57f2005849f218851cb7630169b
Luis <l_meie13@uni-muenster.de>
2020-05-14 09:18:46 +0200

    Fixed Indentation

--- src/main/java/com/group6/group6/repository/RequestRepository.java
+++ src/main/java/com/group6/group6/repository/RequestRepository.java
@@ -1,9 +1,8 @@
 package com.group6.group6.repository;
 
 import com.group6.group6.model.Request;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RequestRepository extends JpaRepository<Request, Long>{
-    public Request findByTitle(String title);
+public interface RequestRepository extends JpaRepository<Request, Long> {
+  public Request findByTitle(String title);
 }
