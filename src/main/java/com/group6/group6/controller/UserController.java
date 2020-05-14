ae6deea78337e67e5b2c10a0f5099e6720da774a
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-05-14 10:38:16 +0200

    display errors per field

--- src/main/java/com/group6/group6/controller/UserController.java
+++ src/main/java/com/group6/group6/controller/UserController.java
@@ -7,15 +7,13 @@ import com.group6.group6.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
@@ -38,25 +36,28 @@ public class UserController {
    * Process registration form
    */
   @PostMapping("/register")
-  public ModelAndView registerUserAccount(
-    @ModelAttribute("user") @Valid User user,
-    HttpServletRequest request,
+  public String registerUserAccount(
+    @Valid User user,
+    BindingResult bindingResult,
     RedirectAttributes redirectAttributes
   ) {
+    if (bindingResult.hasErrors()) {
+      return "user/register";
+    }
+
     try {
       userService.registerNewUserAccount(user);
     } catch (DuplicateUserException e) {
-      ModelAndView mav = new ModelAndView("user/register", "user", user);
       redirectAttributes.addFlashAttribute("message", e.getMessage());
       redirectAttributes.addFlashAttribute("messageType", "error");
 
-      return mav;
+      return "user/register";
     }
 
     redirectAttributes.addFlashAttribute("message", "You have successfully registered!");
     redirectAttributes.addFlashAttribute("messageType", "success");
 
-    return new ModelAndView("redirect:/users/login");
+    return "redirect:/users/login";
   }
 
 }
