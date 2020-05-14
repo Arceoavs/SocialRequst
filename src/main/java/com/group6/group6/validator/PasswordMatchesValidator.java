86edbb18ce0cb221a932a70a30ba505bb192e3bd
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-05-14 10:38:02 +0200

    bind passwordMatches to passwordConfirmation

--- src/main/java/com/group6/group6/validator/PasswordMatchesValidator.java
+++ src/main/java/com/group6/group6/validator/PasswordMatchesValidator.java
@@ -8,14 +8,27 @@ import com.group6.group6.validator.annotation.PasswordMatches;
 
 public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
 
+  private String message;
+
   @Override
-  public void initialize(PasswordMatches constraintAnnotation) {}
+  public void initialize(PasswordMatches constraintAnnotation) {
+    this.message = constraintAnnotation.message();
+  }
 
   @Override
   public boolean isValid(Object object, ConstraintValidatorContext context) {
     User user = (User) object;
+    boolean isValid = user.getPassword().equals(user.getPasswordConfirmation());
 
-    return user.getPassword().equals(user.getPasswordConfirmation());
+    if (!isValid) {
+      context
+        .buildConstraintViolationWithTemplate(this.message)
+        .addPropertyNode("passwordConfirmation")
+        .addConstraintViolation()
+        .disableDefaultConstraintViolation();
+    }
+
+    return isValid;
   }
 
 }
