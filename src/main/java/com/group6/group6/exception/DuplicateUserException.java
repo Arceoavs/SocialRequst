8c576c4d38d4bcd2eb9496e878457da8eb47734e
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-05-15 11:54:20 +0200

    fix indentation

--- src/main/java/com/group6/group6/exception/DuplicateUserException.java
+++ src/main/java/com/group6/group6/exception/DuplicateUserException.java
@@ -2,22 +2,22 @@ package com.group6.group6.exception;
 
 public class DuplicateUserException  extends RuntimeException {
 
-    private static final long serialVersionUID = 5861310537366287163L;
+  private static final long serialVersionUID = 5861310537366287163L;
 
-    public DuplicateUserException() {
-        super();
-    }
+  public DuplicateUserException() {
+    super();
+  }
 
-    public DuplicateUserException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
+  public DuplicateUserException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
 
-    public DuplicateUserException(final String message) {
-        super(message);
-    }
+  public DuplicateUserException(final String message) {
+    super(message);
+  }
 
-    public DuplicateUserException(final Throwable cause) {
-        super(cause);
-    }
+  public DuplicateUserException(final Throwable cause) {
+    super(cause);
+  }
 
 }
