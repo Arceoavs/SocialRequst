f37b1704c8cfba3f0931adc0edc8d7095ed0be30
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-05-16 11:10:47 +0200

    beautify code a bit

--- src/main/java/com/group6/group6/repository/RequestRepository.java
+++ src/main/java/com/group6/group6/repository/RequestRepository.java
@@ -4,6 +4,7 @@ import com.group6.group6.model.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RequestRepository extends JpaRepository<Request, Long> {
+
   public Request findByTitle(String title);
-  
+
 }
