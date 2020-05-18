240dbca7e15ea9bd9b103490888bb2f460b40ef9
ClaasCas <claascassens@t-online.de>
2020-05-18 18:43:26 +0200

    Added topics to request_card

--- src/main/java/com/group6/group6/service/SearchService.java
+++ src/main/java/com/group6/group6/service/SearchService.java
@@ -1,13 +1,12 @@
 package com.group6.group6.service;
 
 import com.group6.group6.model.Request;
+import com.group6.group6.model.Topic;
 import com.group6.group6.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class SearchService {
@@ -25,9 +24,14 @@ public class SearchService {
       return Collections.emptyList();
     }
 
+    Set<Topic> topics = new HashSet<>();
+
+    topics.add(new Topic("Sports"));
+    topics.add(new Topic("Test"));
+
     List<Request> resultList = new ArrayList<>();
-    resultList.add(new Request("R1", "Desc 1", 5.23423F, 4.23534F));
-    resultList.add(new Request("R2", "Desc 124", 5.212323F, 4.2312334F));
+    resultList.add(new Request("R1", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed", 5.23423F, 4.23534F, topics));
+    resultList.add(new Request("R2", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,", 5.212323F, 4.2312334F));
 
     return resultList;
   }
