0c1aab47520500ff67a2d48e80d548c506063668
ClaasCas <claascassens@t-online.de>
2020-05-18 19:50:55 +0200

    Removed intermediate tests

--- src/main/java/com/group6/group6/service/SearchService.java
+++ src/main/java/com/group6/group6/service/SearchService.java
@@ -24,21 +24,6 @@ public class SearchService {
       return Collections.emptyList();
     }
 
-
-    if (requestRepository.findByTitleIsContainingIgnoreCase("R").size() <= 0) {
-      // create some test data
-      Set<Topic> topics = new HashSet<>();
-
-      topics.add(new Topic("Sports"));
-      topics.add(new Topic("Test"));
-
-      List<Request> testList = new ArrayList<>();
-      testList.add(new Request("R1", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed", 5.23423F, 4.23534F, topics));
-      testList.add(new Request("R2", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, st ea rebum. Stet clita kasd gubergren,", 5.212323F, 4.2312334F));
-
-      requestRepository.saveAll(testList);
-    }
-
     List<Request> resultsByTitle = requestRepository.findByTitleIsContainingIgnoreCase(query);
     List<Request> resultsByDescription = requestRepository.findByDescriptionIsContainingIgnoreCase(query);
 
