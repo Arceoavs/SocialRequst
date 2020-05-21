90d01999e31331bf9db805a77b7bf9db50a78cb6
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-05-21 09:47:47 +0200

    make search an AJAX action and show initial request on homepage

--- src/main/java/com/group6/group6/controller/SearchController.java
+++ src/main/java/com/group6/group6/controller/SearchController.java
@@ -1,15 +1,15 @@
 package com.group6.group6.controller;
 
 import com.group6.group6.model.Request;
-import com.group6.group6.model.User;
 import com.group6.group6.service.SearchService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
@@ -24,19 +24,16 @@ public class SearchController {
    * Method to read the request parameter q, task the SearchService with fetching results
    * and rendering the html page with the results.
    *
-   * @param q the search query string, retrieved from the request
+   * @param query the search query string, retrieved from the request
    * @param model
    * @return html location
    */
-  @RequestMapping
-  public String showSearchPage(
-          @RequestParam(required = false) String q,
-          Model model) {
-    model.addAttribute("searchQuery", q);
+  @GetMapping
+  @PostMapping
+  public String showSearchPage(@RequestParam(required = false, name = "q") String query, Model model) {
+    List<Request> results = searchService.getRequests(query);
+    model.addAttribute("results", results);
 
-    List<Request> requestList = searchService.getRequests(q);
-    model.addAttribute("searchResult", requestList);
-
-    return "search/index";
+    return "search/results :: results";
   }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   initializeNavbar();
   initializeFlashMessages();
+  initializeSearch();
 });
 
 function initializeNavbar() {
@@ -23,11 +24,76 @@ function initializeFlashMessages() {
 
   if (flashMessages.length > 0) {
     flashMessages.forEach((el) => {
-      setTimeout(() => el.remove(), 3000);
+      setTimeout(() => el.remove(), 5000);
 
       el.querySelector('.delete').addEventListener('click', () => {
         el.remove();
       });
     })
   }
+}
+
+function initializeSearch() {
+  const $searchForm = document.querySelector('form#search-form');
+  if (!$searchForm) return;
+
+  $searchForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    $resultsWrapper = document.querySelector('#seach-results-wrapper');
+
+    // add loading spinner
+    $resultsWrapper.innerHTML = `
+      <div class="sk-circle" style="left: 50%;">
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+        <div class="sk-circle-dot"></div>
+      </div>
+    `;
+
+    let url = new URL($searchForm.action);
+    url.search = new URLSearchParams({ q: $searchForm.querySelector('[name="q"]').value });
+
+    try {
+      const response = await fetch(url);
+      $resultsWrapper.innerHTML = await response.text();
+    } catch(error) {
+      console.log('Something went wrong', error);
+      createFlashMessage('error', `Something went wrong: ${error}`)
+    }
+  });
+}
+
+function createFlashMessage(type, message) {
+  let flashMessage = document.createElement('div');
+  flashMessage.classList.add(['flash-message', 'notification']);
+  switch (type) {
+    case 'success':
+      flashMessage.classList.add('is-success');
+      break;
+    case 'error':
+      flashMessage.classList.add('is-danger');
+      break;
+    default:
+      flashMessage.classList.add('is-info');
+      break;
+  }
+
+  flashMessage.innerHTML = `
+    <button class="delete"></button>
+    <span>${message}</span>
+  `;
+
+  document.body.prepend(flashMessage);
+  setTimeout(() => flashMessage.remove(), 5000);
+  flashMessage.querySelector('.delete').addEventListener('click', () => flashMessage.remove());
 }
