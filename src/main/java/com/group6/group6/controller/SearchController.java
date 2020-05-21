b5394ee303ae17d230f34190fe71d7bd9cef728d
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-05-21 11:12:50 +0200

    remove unnecessary PostMapping

--- src/main/java/com/group6/group6/controller/SearchController.java
+++ src/main/java/com/group6/group6/controller/SearchController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -29,7 +28,6 @@ public class SearchController {
    * @return html location
    */
   @GetMapping
-  @PostMapping
   public String showSearchPage(@RequestParam(required = false, name = "q") String query, Model model) {
     List<Request> results = searchService.getRequests(query);
     model.addAttribute("results", results);
