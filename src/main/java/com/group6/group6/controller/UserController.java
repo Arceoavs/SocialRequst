c0f699cbc481ab6490472fa80d7997fa00b67c2e
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-05-23 14:40:56 +0200

    update user service, controller according to new register, update actions

--- src/main/java/com/group6/group6/controller/UserController.java
+++ src/main/java/com/group6/group6/controller/UserController.java
@@ -1,14 +1,24 @@
 package com.group6.group6.controller;
 
+import com.group6.group6.auth.CurrentUser;
 import com.group6.group6.exception.DuplicateUserException;
+import com.group6.group6.form.RegisterUserForm;
+import com.group6.group6.form.UserProfileForm;
 import com.group6.group6.model.User;
+import com.group6.group6.repository.UserRepository;
+import com.group6.group6.service.TopicService;
+import com.group6.group6.service.UserAuthenticationService;
 import com.group6.group6.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
@@ -21,7 +31,16 @@ import javax.validation.Valid;
 public class UserController extends ApplicationController {
 
   @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
   private UserService userService;
+
+  @Autowired
+  private TopicService topicService;
+
+  @Autowired
+  private UserAuthenticationService authenticationService;
 
   /*
    * Deliver registration page
@@ -34,7 +53,7 @@ public class UserController extends ApplicationController {
       return redirectToBack(request).orElse("redirect:/");
     }
 
-    model.addAttribute("user", new User());
+    model.addAttribute("userForm", new RegisterUserForm());
     return "user/register";
   }
   
@@ -43,7 +62,7 @@ public class UserController extends ApplicationController {
    */
   @PostMapping("/register")
   public String registerUserAccount(
-    @Valid User user,
+    @ModelAttribute("userForm") @Valid RegisterUserForm userForm,
     BindingResult bindingResult,
     RedirectAttributes redirectAttributes
   ) {
@@ -52,7 +71,7 @@ public class UserController extends ApplicationController {
     }
 
     try {
-      userService.registerNewUserAccount(user);
+      userService.registerNewUserAccount(userForm);
     } catch (DuplicateUserException e) {
       if (e.getMessage().contains("username already exists")) {
         bindingResult.rejectValue("username", "", e.getMessage());
@@ -68,5 +87,83 @@ public class UserController extends ApplicationController {
 
     return "redirect:/users/login";
   }
+
+  @GetMapping("/me")
+  public String showUserProfile(Model model) {
+    User currentUser = userRepository.getOne(
+      ((CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUser().getId()
+    );
+    model.addAttribute("user", currentUser);
+    model.addAttribute("userForm", new UserProfileForm(
+      currentUser.getId(),
+      currentUser.getUsername(),
+      currentUser.getEmail(),
+      currentUser.getSpecialties(),
+      currentUser.getLat(),
+      currentUser.getLng()
+    ));
+    model.addAttribute("availableTopics", topicService.getAllTopics());
+
+    return "user/me";
+  }
+
+  @PostMapping("/me")
+  public String updateUser(
+    @ModelAttribute("userForm") @Valid UserProfileForm userForm,
+    BindingResult bindingResult,
+    Model model,
+    RedirectAttributes redirectAttributes
+  ) {
+    User currentUser = userRepository.getOne(
+      ((CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUser().getId()
+    );
+
+    if (bindingResult.hasErrors()) {
+      model.addAttribute("availableTopics", topicService.getAllTopics());
+      model.addAttribute("user", currentUser);
+
+      return "user/me";
+    }
+
+    try {
+      userService.updateUser(userForm);
+    } catch (DuplicateUserException e) {
+      if (e.getMessage().contains("username already exists")) {
+        bindingResult.rejectValue("username", "", e.getMessage());
+      } else if (e.getMessage().contains("email already exists")) {
+        bindingResult.rejectValue("email", "", e.getMessage());
+      }
+      model.addAttribute("availableTopics", topicService.getAllTopics());
+      model.addAttribute("user", currentUser);
+
+      return "user/me";
+    }
+
+    // isAuthenticated() is false. It has to be true
+    // this is necessary, so the username is reloaded in case it was changed by the form
+    Authentication authentication = new UsernamePasswordAuthenticationToken(
+      authenticationService.loadUserByUsername(userForm.getUsername()),
+      currentUser.getPassword()
+    );
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    redirectAttributes.addFlashAttribute("message", "You have successfully updated your account!");
+    redirectAttributes.addFlashAttribute("messageType", "success");
+
+    return "redirect:/users/me";
+  }
+
+  // @PostMapping("/update-password")
+  // @ResponseBody
+  // public Map<String, Object> updatePassword(
+  //   @Valid UpdatePasswordForm passwordForm,
+  //   BindingResult bindingResult
+  // ) {
+  //   User currentUser = userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+  //   model.addAttribute("passwordForm", new UpdatePasswordForm(currentUser.getPassword()));
+  //   Map<String, Object> responseJson = new LinkedHashMap<>();
+
+  //   return "";
+  // }
 
 }
--- src/main/java/com/group6/group6/service/UserService.java
+++ src/main/java/com/group6/group6/service/UserService.java
@@ -1,11 +1,13 @@
 package com.group6.group6.service;
 
 import com.group6.group6.exception.DuplicateUserException;
+import com.group6.group6.form.RegisterUserForm;
+import com.group6.group6.form.UserProfileForm;
 import com.group6.group6.model.User;
 import com.group6.group6.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,22 +15,50 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
   @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  @Autowired
   private UserRepository userRepository;
- 
+
   @Transactional
-  public User registerNewUserAccount(User user) throws DuplicateUserException { 
-    if (emailExists(user.getEmail())) {
-      throw new DuplicateUserException("An account with that email already exists: " + user.getEmail());
+  public User registerNewUserAccount(RegisterUserForm userForm) throws DuplicateUserException {
+    if (emailExists(userForm.getEmail())) {
+      throw new DuplicateUserException("An account with that email already exists: " + userForm.getEmail());
     }
 
-    if (usernameExists(user.getUsername())) {
-      throw new DuplicateUserException("An account with that username already exists: " + user.getUsername());
+    if (usernameExists(userForm.getUsername())) {
+      throw new DuplicateUserException("An account with that username already exists: " + userForm.getUsername());
     }
 
-    String encryptedPassword = (new BCryptPasswordEncoder()).encode(user.getPassword());
-    user.setPassword(encryptedPassword);
-    user.setPasswordConfirmation(encryptedPassword);
-    
+    String encryptedPassword = passwordEncoder.encode(userForm.getPassword());
+    User user = new User(
+      userForm.getUsername(),
+      userForm.getEmail(),
+      encryptedPassword,
+      userForm.getLat(),
+      userForm.getLng()
+    );
+
+    return userRepository.save(user);
+  }
+
+  @Transactional
+  public User updateUser(UserProfileForm userForm) throws DuplicateUserException {
+    if (emailExists(userForm.getEmail(), userForm.getId())) {
+      throw new DuplicateUserException("An account with that email already exists: " + userForm.getEmail());
+    }
+
+    if (usernameExists(userForm.getUsername(), userForm.getId())) {
+      throw new DuplicateUserException("An account with that username already exists: " + userForm.getUsername());
+    }
+
+    User user = userRepository.getOne(userForm.getId());
+    user.setUsername(userForm.getUsername());
+    user.setEmail(userForm.getEmail());
+    user.setSpecialties(userForm.getSpecialties());
+    user.setLat(userForm.getLat());
+    user.setLat(userForm.getLng());
+
     return userRepository.save(user);
   }
 
@@ -38,6 +68,14 @@ public class UserService {
 
   private boolean usernameExists(String username) {
     return userRepository.findByUsername(username) != null;
+  }
+
+  private boolean emailExists(String email, Long id) {
+    return userRepository.otherUserExistsWithEmail(email, id);
+  }
+
+  private boolean usernameExists(String username, Long id) {
+    return userRepository.otherUserExistsWithUsername(username, id);
   }
 
 }
