16ee77997b1242e343b1f22517740acc1059a424
Arnold Arz <arnold.arz@uni-muenster.de>
2020-06-27 21:06:30 +0200

    🍱 adding leaderboard basic file structure

---
+++ leaderboard/src/test/java/de/wwu/acse/group6/leaderboard/LeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wwu.acse.group6.leaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
