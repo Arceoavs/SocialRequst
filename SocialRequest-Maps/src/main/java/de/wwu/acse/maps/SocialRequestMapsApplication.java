255104892cd04c39cc31c5f7dae8ecb7810a4254
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-06-28 09:32:23 +0200

    add basic architecture construct for maps application

---
+++ SocialRequest-Maps/src/main/java/de/wwu/acse/maps/SocialRequestMapsApplication.java
@@ -0,0 +1,13 @@
+package de.wwu.acse.maps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialRequestMapsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SocialRequestMapsApplication.class, args);
+  }
+
+}
