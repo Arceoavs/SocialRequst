9387b95d6edbd2b608d9e9e18fcad98c025769a5
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-06-28 10:28:32 +0200

    remove fulfillment model and repo

--- SocialRequest-Leaderboard/src/main/java/de/wwu/acse/leaderboard/repository/FulfillmentRepository.java
+++
@@ -1,8 +0,0 @@
-package de.wwu.acse.leaderboard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import de.wwu.acse.leaderboard.model.Fulfillment;
-
-public interface FulfillmentRepository extends JpaRepository<Fulfillment, Long> {
-}
