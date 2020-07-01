d4fbe277d6a5063dbc99af34f92b4fc5a9176dd4
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-07-01 10:58:27 +0200

    ✨ throw 422 HTTP status if request body is invalid

--- SocialRequest-Maps/src/main/java/de/wwu/acse/maps/controller/RouteController.java
+++ SocialRequest-Maps/src/main/java/de/wwu/acse/maps/controller/RouteController.java
@@ -1,6 +1,5 @@
 package de.wwu.acse.maps.controller;
 
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import de.wwu.acse.maps.model.Coordinates;
+import de.wwu.acse.maps.exception.UnprocessableEntityException;
 import de.wwu.acse.maps.model.Address;
 import de.wwu.acse.maps.model.Distance;
 import de.wwu.acse.maps.model.Route;
@@ -29,7 +29,7 @@ public class RouteController {
   @PostMapping(value = "/geocode", consumes = "application/json", produces = "application/json")
   public Coordinates getCoordinates(@Valid @RequestBody Address address, BindingResult bindingResult) {
     if (bindingResult.hasErrors()){
-
+      throw new UnprocessableEntityException();
     }
     return tomTomService.getCoordinates(address.getAddress());
   }
@@ -37,7 +37,7 @@ public class RouteController {
   @PostMapping(value = "/route", consumes = "application/json", produces = "application/json")
   public Route getRoute(@Valid @RequestBody RouteCoordinates routeCoordinates, BindingResult bindingResult){
     if (bindingResult.hasErrors()){
-      
+      throw new UnprocessableEntityException();
     }
     return tomTomService.getRoute(routeCoordinates.getOrigin(), routeCoordinates.getDestination());
   }
@@ -45,7 +45,7 @@ public class RouteController {
   @PostMapping(value = "/distance", consumes = "application/json", produces = "application/json")
   public Distance getDistance(@Valid @RequestBody RouteCoordinates routeCoordinates, BindingResult bindingResult){
     if (bindingResult.hasErrors()){
-      
+      throw new UnprocessableEntityException();
     }
     return tomTomService.getDistance(routeCoordinates.getOrigin(), routeCoordinates.getDestination());
   }
