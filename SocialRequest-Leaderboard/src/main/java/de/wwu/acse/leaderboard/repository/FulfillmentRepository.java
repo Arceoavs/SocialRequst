863b9c131685096e8abfd194299ed2f1872d2bd0
Jonathan Heins <jonathan.heins@gmail.com>
2020-07-04 13:26:46 +0200

    :sparkles: add queries for monthly leaderboard

--- SocialRequest-Leaderboard/src/main/java/de/wwu/acse/leaderboard/repository/FulfillmentRepository.java
+++ SocialRequest-Leaderboard/src/main/java/de/wwu/acse/leaderboard/repository/FulfillmentRepository.java
@@ -10,7 +10,8 @@ import java.util.List;
 public interface FulfillmentRepository extends JpaRepository<Fulfillment, Long> {
 
   /**
-   * Searches for the useres that fullfiled requests ordered by the sum of the kilometeres traveled
+   * Searches for the useres that fullfiled requests
+   * ordered by the sum of the kilometeres traveled
    * @return list of usernames
    */
   @Query("SELECT f.username FROM Fulfillment f " +
@@ -19,7 +20,8 @@ public interface FulfillmentRepository extends JpaRepository<Fulfillment, Long> 
   public List<String> allTimeLeaderboardUsers();
 
   /**
-   * Searches for the amount of kilometeres traveled by useres that fullfiled requests ordered by the sum of the kilometeres traveled
+   * Searches for the amount of kilometeres traveled by useres
+   * that fullfiled requests ordered by the sum of the kilometeres traveled
    * @return list of kilometeres traveled
    */
   @Query("SELECT SUM(f.distance) from Fulfillment f " +
@@ -27,7 +29,28 @@ public interface FulfillmentRepository extends JpaRepository<Fulfillment, Long> 
     "ORDER BY SUM(f.distance) DESC")
   public List<Float> allTimeLeaderboardKilometers();
 
+  /**
+   * Searches for the useres that fullfiled requests in this month
+   * ordered by the sum of the kilometeres traveled
+   * @return list of usernames
+   */
+  @Query("SELECT f.username FROM Fulfillment f " +
+    "WHERE MONTH(f.fulfilledAt) = MONTH(CURRENT_TIMESTAMP) " +
+    "GROUP BY f.username " +
+    "ORDER BY SUM(f.distance) DESC")
+  public List<String> monthlyLeaderboardUsers();
 
+  /**
+   * Searches for the amount of kilometeres traveled by useres
+   * that fullfiled requests in this month
+   * ordered by the sum of the kilometeres traveled
+   * @return list of kilometeres traveled
+   */
+  @Query("SELECT SUM(f.distance) from Fulfillment f " +
+    "WHERE MONTH( f.fulfilledAt ) = MONTH( CURRENT_TIMESTAMP ) " +
+    "GROUP BY f.username " +
+    "ORDER BY SUM(f.distance) DESC")
+  public List<Float> monthlyLeaderboardKilometers();
 
 
 }
